4124d26c5a66ad040711ac2347a897b623fdaec8
Rakshith Acharya <rakshixh@gmail.com>
2024-04-08 17:14:06 +0530

    404 not found page
    Co-authored-by: RakshaBKottari <83587982+RakshaBKottari@users.noreply.github.com>

--- src/pages/notfound.jsx
+++ src/pages/notfound.jsx
@@ -5,11 +5,11 @@ const NotFoundPage = () => {
   return (
     <section className="text-center flex flex-col justify-center items-center h-96">
       <FaExclamationTriangle className="text-yellow-400 text-6xl mb-4" />
-      <h1 className="text-6xl font-bold mb-4">404 Not Found</h1>
-      <p className="text-xl mb-5">This page does not exist</p>
+      <h1 className="text-6xl font-bold mb-4 text-yellow-300">404 Not Found</h1>
+      <p className="text-xl mb-5 text-yellow-300">This page does not exist</p>
       <Link
         to="/"
-        className="text-white bg-indigo-700 hover:bg-indigo-900 rounded-md px-3 py-2 mt-4"
+        className="text-cyan-950 font-bold bg-cyan-500 hover:bg-yellow-300 rounded-md px-10 py-3 mt-4"
       >
         Go Back
       </Link>
