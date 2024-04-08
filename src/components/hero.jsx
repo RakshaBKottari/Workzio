99ba073a3e4f083b2241a248b8a7ed431a8ccc6a
Rakshith Acharya <rakshixh@gmail.com>
2024-04-08 16:42:41 +0530

    mobile res padding

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -7,10 +7,10 @@ const hero = ({ title, subtitle }) => {
       <section className="bg-cyan-700 py-20 mb-4">
         <div className="flex flex-row items-center justify-center h-full flex-wrap mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
           <div className="text-left max-w-3xl mx-auto">
-            <h1 className="text-4xl font-extrabold text-white sm:text-5xl md:text-6xl">
+            <h1 className="text-4xl font-extrabold text-white sm:text-5xl md:text-6xl px-5">
               {title}
             </h1>
-            <p className="max-w-xl my-4 text-xl text-white">{subtitle}</p>
+            <p className="max-w-xl my-4 text-xl text-white px-5">{subtitle}</p>
           </div>
 
           <div className="mx-auto">
--- src/components/homecards.jsx
+++ src/components/homecards.jsx
@@ -4,11 +4,11 @@ import Card from "./card";
 function homecards() {
   return (
     <div>
-      <section class="py-4">
-        <div class="container-xl lg:container m-auto">
-          <div class="grid grid-cols-1 md:grid-cols-2 gap-8 px-40 rounded-lg">
+      <section className="py-4">
+        <div className="container mx-auto">
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-8 md:px-8 px-5 lg:px-20 xl:px-40">
             <Card bg="bg-cyan-100">
-              <h2 class="text-2xl font-bold">For Developers</h2>
+              <h2 className="text-2xl font-bold">For Developers</h2>
               <p className="mt-2 mb-4">
                 Calling all developers! Explore exciting job opportunities
                 tailored just for you. Click the 'Browse Jobs' button below to
@@ -17,14 +17,14 @@ function homecards() {
               </p>
               <a
                 href="/jobs.html"
-                class="inline-block bg-cyan-900 text-white rounded-lg px-4 py-2 hover:bg-gray-600"
+                className="inline-block bg-cyan-900 text-white rounded-lg px-4 py-2 hover:bg-gray-600"
               >
                 Browse Jobs
               </a>
             </Card>
             <Card bg="bg-cyan-100">
-              <h2 class="text-2xl font-bold">For Employers</h2>
-              <p class="mt-2 mb-4">
+              <h2 className="text-2xl font-bold">For Employers</h2>
+              <p className="mt-2 mb-4">
                 Attract top talent for your team! Post job openings and connect
                 with skilled professionals eager to join your company. Click the
                 'Post a Job' button below to get started and find the perfect
@@ -32,7 +32,7 @@ function homecards() {
               </p>
               <a
                 href="/add-job.html"
-                class="inline-block bg-cyan-900 text-white rounded-lg px-4 py-2 hover:bg-gray-600"
+                className="inline-block bg-cyan-900 text-white rounded-lg px-4 py-2 hover:bg-gray-600"
               >
                 Add Job
               </a>
